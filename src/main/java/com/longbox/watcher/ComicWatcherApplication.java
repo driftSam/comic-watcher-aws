eb62e149892de7a8a190edd237f6d309ef8cd4e7
pete <pjjuliana216@gmail.com>
2019-07-28 12:35:29 -0400

    Initial empty project

---
+++ src/main/java/com/longbox/watcher/ComicWatcherApplication.java
@@ -0,0 +1,13 @@
+package com.longbox.watcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicWatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicWatcherApplication.class, args);
+	}
+
+}
