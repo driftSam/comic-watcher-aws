09ab6b0f61033be5bc3d4a81565289ee253fd09b
Pete <pjuliana@ncmec.org>
2019-07-29 11:48:01 -0400

    Add docker-compose. Remover MessageListenerContainer

--- src/main/java/com/longbox/watcher/ComicWatcherApplication.java
+++ src/main/java/com/longbox/watcher/ComicWatcherApplication.java
@@ -6,9 +6,7 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -47,15 +45,7 @@ public class ComicWatcherApplication implements CommandLineRunner {
 	Binding binding(Queue queue, DirectExchange exchange) {
 		return BindingBuilder.bind(queue).to(exchange).with("comic.found");
 	}
-
-	@Bean
-	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory) {
-		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-		container.setConnectionFactory(connectionFactory);
-		container.setQueueNames(queueName);
-		return container;
-	}
-
+	
 	private static Logger LOG = LoggerFactory.getLogger(ComicWatcherApplication.class);
 
 	public static void main(String[] args) {
