ad5d07211853119f4ff56c0c22e617d99ee59963
pete <pjjuliana216@gmail.com>
2019-12-22 22:26:32 -0500

    Remove rabbitmq config

--- src/main/java/com/longbox/watcher/ComicWatcherApplication.java
+++ src/main/java/com/longbox/watcher/ComicWatcherApplication.java
@@ -2,15 +2,10 @@ package com.longbox.watcher;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 import com.longbox.watcher.service.ComicWatcherService;
 
@@ -19,25 +14,6 @@ public class ComicWatcherApplication implements CommandLineRunner {
 
 	@Autowired
 	ComicWatcherService service;
-
-	static final String directExchangeName = "comic-exchange";
-
-	static final String queueName = "comic.found";
-
-	@Bean
-	Queue queue() {
-		return new Queue(queueName, false);
-	}
-
-	@Bean
-	DirectExchange exchange() {
-		return new DirectExchange(directExchangeName);
-	}
-
-	@Bean
-	Binding binding(Queue queue, DirectExchange exchange) {
-		return BindingBuilder.bind(queue).to(exchange).with("found");
-	}
 
 	private static Logger LOG = LoggerFactory.getLogger(ComicWatcherApplication.class);
 
